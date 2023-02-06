861f3eede9dad0903e8b11a7372749058ad9b941
Pin Chen <pamyc1876@gmail.com>
2023-02-06 13:23:32 -0500

    Update the lobby schema.

--- models/lobby.js
+++ models/lobby.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema
 const lobbySchema = new Schema({
   name: String,
   content: String,
-  chatroom: { type: Schema.Types.ObjectId, ref: 'Chatroom' },
+  mainroom: { type: Schema.Types.ObjectId, ref: 'Chatroom' },
   gamerooms: [{ type: Schema.Types.ObjectId, ref: 'Gameroom'}],
+  chatrooms: [{ type: Schema.Types.ObjectId, ref: 'Chatroom'}],
   members: [{ type: Schema.Types.ObjectId, ref: 'Profile'}]
 }, {
   timestamps: true,
