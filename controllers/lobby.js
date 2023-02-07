32b20d9fd2a83986d63eb33d6673a4abc6dd1b0d
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-02-06 19:13:07 -0500

    update lobby show function to pupulate messages

--- controllers/lobby.js
+++ controllers/lobby.js
@@ -28,6 +28,7 @@ Lobby.find({})
 function show(req, res) {
 Lobby.findById(req.params.id)
 .populate('mainroom')
+.populate({path:"mainroom", populate: "messages"})
 .populate('gamerooms')
 .populate('members')
 .then(lobby => {
