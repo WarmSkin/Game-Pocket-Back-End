d8f383b425f388b3b566ccf33075a03c6c665dba
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-09 10:57:14 -0500

    add route to leave lobby

--- routes/lobby.js
+++ routes/lobby.js
@@ -13,6 +13,7 @@ router.get('/', checkAuth, lobbyCtrl.index)
 router.get('/:id', checkAuth, lobbyCtrl.show)
 router.put('/:id', checkAuth, lobbyCtrl.update)
 router.put('/:id/join-lobby', checkAuth, lobbyCtrl.joinLobby)
+router.put('/:id/leave-lobby', checkAuth, lobbyCtrl.leaveLobby)
 router.put('/:lid/:cid/add-chatroom', lobbyCtrl.addChatroom)
 router.delete('/:id', checkAuth, lobbyCtrl.delete)
 
